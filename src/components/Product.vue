3d9f9af269da524dd2c2b17b53334fc08b1789e9
wmartzh <wmartzh@gmail.com>
2021-04-06 18:10:49 -0600

    [+] Added Groupbuys

---
+++ src/components/Product.vue
@@ -0,0 +1,40 @@
+<template>
+  <vs-card-group>
+    <vs-card type="4" v-for="(product, index) in products" :key="index">
+      <template #title>
+        <h3>{{ product.title }}</h3>
+      </template>
+      <template #img>
+        <img
+          height="250px"
+          width="250px"
+          :src="product.imgUrl"
+          alt="groupbuy"
+        />
+      </template>
+      <template #text>
+        <p>
+          {{ product.description }}
+        </p>
+      </template>
+      <template #interactions>
+        <vs-button success icon>
+          {{ product.price }}
+        </vs-button>
+        <vs-button class="btn-chat" shadow primary>
+          <i class="bx bxs-cart-add"></i>
+        </vs-button>
+      </template>
+    </vs-card>
+  </vs-card-group>
+</template>
+
+<script>
+export default {
+  props: {
+    products: Array,
+  },
+};
+</script>
+
+<style></style>
--- src/pages/Groupbuys.vue
+++ src/pages/Groupbuys.vue
@@ -1,11 +1,115 @@
 <template>
-  <div>
-    <h1>Group Buys</h1>
-  </div>
+  <b-container fluid>
+    <b-row>
+      <b-col>
+        <h1>Comming Groupbuys</h1>
+      </b-col>
+    </b-row>
+    <b-row>
+      <Product :products="products"></Product>
+    </b-row>
+    <b-row>
+      <b-col>
+        <h1>In Production</h1>
+      </b-col>
+    </b-row>
+    <b-row>
+      <Product :products="products"></Product>
+    </b-row>
+    <b-row>
+      <b-col>
+        <h1>End</h1>
+      </b-col>
+    </b-row>
+    <b-row>
+      <Product :products="products"></Product>
+    </b-row>
+  </b-container>
 </template>
 
 <script>
-export default {};
+import Product from "../components/Product";
+export default {
+  components: {
+    Product,
+  },
+  data() {
+    return {
+      products: [
+        {
+          title: "EZ223",
+          imgUrl:
+            "https://alitools.io/en/showcase/image?url=https%3A%2F%2Fae01.alicdn.com%2Fkf%2FHLB1XyPaUwHqK1RjSZFEq6AGMXXai%2F1-set-SA-Key-caps-PBT-Mechanical-Keyboard-Keycaps-For-Filco-Cherry-EVA-key-cap.jpg_480x480.jpg",
+          description: "Comming Soon",
+          price: "$200",
+        },
+        {
+          title: "EZ223",
+          imgUrl:
+            "https://alitools.io/en/showcase/image?url=https%3A%2F%2Fae01.alicdn.com%2Fkf%2FHLB1XyPaUwHqK1RjSZFEq6AGMXXai%2F1-set-SA-Key-caps-PBT-Mechanical-Keyboard-Keycaps-For-Filco-Cherry-EVA-key-cap.jpg_480x480.jpg",
+          description: "Comming Soon",
+          price: "$200",
+        },
+        {
+          title: "EZ223",
+          imgUrl:
+            "https://alitools.io/en/showcase/image?url=https%3A%2F%2Fae01.alicdn.com%2Fkf%2FHLB1XyPaUwHqK1RjSZFEq6AGMXXai%2F1-set-SA-Key-caps-PBT-Mechanical-Keyboard-Keycaps-For-Filco-Cherry-EVA-key-cap.jpg_480x480.jpg",
+          description: "Comming Soon",
+          price: "$200",
+        },
+        {
+          title: "EZ223",
+          imgUrl:
+            "https://alitools.io/en/showcase/image?url=https%3A%2F%2Fae01.alicdn.com%2Fkf%2FHLB1XyPaUwHqK1RjSZFEq6AGMXXai%2F1-set-SA-Key-caps-PBT-Mechanical-Keyboard-Keycaps-For-Filco-Cherry-EVA-key-cap.jpg_480x480.jpg",
+          description: "Comming Soon",
+          price: "$200",
+        },
+        {
+          title: "EZ223",
+          imgUrl:
+            "https://alitools.io/en/showcase/image?url=https%3A%2F%2Fae01.alicdn.com%2Fkf%2FHLB1XyPaUwHqK1RjSZFEq6AGMXXai%2F1-set-SA-Key-caps-PBT-Mechanical-Keyboard-Keycaps-For-Filco-Cherry-EVA-key-cap.jpg_480x480.jpg",
+          description: "Comming Soon",
+          price: "$200",
+        },
+        {
+          title: "EZ223",
+          imgUrl:
+            "https://alitools.io/en/showcase/image?url=https%3A%2F%2Fae01.alicdn.com%2Fkf%2FHLB1XyPaUwHqK1RjSZFEq6AGMXXai%2F1-set-SA-Key-caps-PBT-Mechanical-Keyboard-Keycaps-For-Filco-Cherry-EVA-key-cap.jpg_480x480.jpg",
+          description: "Comming Soon",
+          price: "$200",
+        },
+        {
+          title: "EZ223",
+          imgUrl:
+            "https://i.pinimg.com/originals/6d/78/9b/6d789b3aa8d89293dcba2c3e40945f50.jpg",
+          description: "Comming Soon",
+          price: "$200",
+        },
+        {
+          title: "EZ223",
+          imgUrl:
+            "https://i.pinimg.com/originals/6d/78/9b/6d789b3aa8d89293dcba2c3e40945f50.jpg",
+          description: "Comming Soon",
+          price: "$200",
+        },
+        {
+          title: "EZ223",
+          imgUrl:
+            "https://i.pinimg.com/originals/6d/78/9b/6d789b3aa8d89293dcba2c3e40945f50.jpg",
+          description: "Comming Soon",
+          price: "$200",
+        },
+        {
+          title: "EZ223",
+          imgUrl:
+            "https://alitools.io/en/showcase/image?url=https%3A%2F%2Fae01.alicdn.com%2Fkf%2FHLB1XyPaUwHqK1RjSZFEq6AGMXXai%2F1-set-SA-Key-caps-PBT-Mechanical-Keyboard-Keycaps-For-Filco-Cherry-EVA-key-cap.jpg_480x480.jpg",
+          description: "Comming Soon",
+          price: "$200",
+        },
+      ],
+    };
+  },
+};
 </script>
 
 <style></style>
